6f40edfa20a31275e62e3b733d71fdb0948968ec
lucaslokchan <lucas.lokchan@gmail.com>
2023-07-01 20:32:01 +0800

    feat(render): Three JS

--- pages/base64/[base64].js
+++ pages/base64/[base64].js
@@ -1,4 +1,9 @@
 import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+let base64Code; // Global variable for base64Code
+let decodedCode; // Global variable for decodedCode
 
 function Base64DecodeUrl(base64) {
   if (base64) {
@@ -10,21 +15,59 @@ function Base64DecodeUrl(base64) {
   return null; // or any default value you prefer
 }
 
+const setupThreeJS = () => {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    if (canvasRef.current) {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      const handleResize = () => {
+        const { innerWidth, innerHeight } = window;
+        camera.aspect = innerWidth / innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(innerWidth, innerHeight);
+      };
+
+      window.addEventListener("resize", handleResize);
+      handleResize(); // Initialize renderer size
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.021;
+        cube.rotation.y += 0.011;
+        cube.rotation.z += 0.001;
+        renderer.render(scene, camera);
+      };
+
+      animate();
+
+      return () => {
+        // Cleanup event listener
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+  return <canvas ref={canvasRef} />;
+};
+
 export default function Page() {
   const router = useRouter();
-  const base64Code = router.query.base64;
+  base64Code = router.query.base64;
+  decodedCode = Base64DecodeUrl(base64Code);
 
-  if (!base64Code) {
-    // Handle the case when base64Code is undefined
-    return <div>Base64 Code is missing or undefined.</div>;
-  }
-
-  const decodedCode = Base64DecodeUrl(base64Code);
-
-  return (
-    <div>
-      <div>Base64 Code: {base64Code}</div>
-      <div>Decoded Code: {decodedCode}</div>
-    </div>
-  );
+  return setupThreeJS();
 }
