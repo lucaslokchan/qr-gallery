ec7cf059c53a7a54e734991c692e552c43200a61
lucaslokchan <lucas.lokchan@gmail.com>
2023-07-02 19:16:42 +0800

    feat(eval): eval cube function

--- pages/base64/[base64].js
+++ pages/base64/[base64].js
@@ -4,12 +4,20 @@ import * as THREE from "three";
 
 let base64Code; // Global variable for base64Code
 let decodedCode; // Global variable for decodedCode
+let cube = null; // Global variable for cube
+
+
+export default function Page() {
+  const router = useRouter();
+  base64Code = router.query.base64;
+  decodedCode = Base64DecodeUrl(base64Code);
+
+  return setupThreeJS();
+}
 
 function Base64DecodeUrl(base64) {
   if (base64) {
     const decodedCode = Buffer.from(base64, "base64").toString("utf-8");
-    console.log("base64: " + typeof base64);
-    console.log("decodedCode: " + decodedCode);
     return decodedCode;
   }
   return null; // or any default value you prefer
@@ -40,10 +48,9 @@ const setupCanvas = (canvasRef, camera, cube) => {
 
 const create3DModel = (scene, camera) => {
   const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
-
   return cube; // Return the cube object
 };
 
@@ -55,14 +62,31 @@ const setupThreeJS = () => {
     0.1,
     1000
   );
-  let cube; // Declare cube variable outside of create3DModel
 
   useEffect(() => {
     if (canvasRef.current) {
       const scene = new THREE.Scene();
 
       const cleanupCanvas = setupCanvas(canvasRef, camera, cube); // Set up canvas and resize handling
-      cube = create3DModel(scene, camera); // Assign the cube object to a variable
+      //base64Code = router.query.base64;
+      base64Code =
+        "KGZ1bmN0aW9uICgpIHsKICBjb25zdCBzY2VuZU9iamVjdCA9IG5ldyBUSFJFRS5PYmplY3QzRCgpOwoKICBjb25zdCBnZW9tZXRyeSA9IG5ldyBUSFJFRS5Cb3hHZW9tZXRyeSgpOwogIGNvbnN0IG1hdGVyaWFsID0gbmV3IFRIUkVFLk1lc2hCYXNpY01hdGVyaWFsKHsgY29sb3I6IDB4ZmZmZjAwIH0pOwogIGNvbnN0IGN1YmUgPSBuZXcgVEhSRUUuTWVzaChnZW9tZXRyeSwgbWF0ZXJpYWwpOwoKICBzY2VuZU9iamVjdC5hZGQoY3ViZSk7CiAgcmV0dXJuIHNjZW5lT2JqZWN0OyAvLyBSZXR1cm4gdGhlIHNjZW5lT2JqZWN0IGNvbnRhaW5pbmcgdGhlIGN1YmUKfSkoKTs=";
+      decodedCode = Base64DecodeUrl(base64Code);
+
+      decodedCode = `(function () {
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        return cube; // Return the cube object
+    })();`;
+      console.log("decodedCode", decodedCode)
+      cube = eval(decodedCode);
+      
+      console.log(cube)
+      // const create3DModel = new Function("scene", "camera", decodedCode);
+      // cube = create3DModel(scene, camera); // Assign the cube object to a variable
+      //cube = create3DModel(scene, camera); // Assign the cube object to a variable
 
       camera.position.z = 5;
 
@@ -86,13 +110,10 @@ const setupThreeJS = () => {
     }
   }, []);
 
-  return <canvas ref={canvasRef} />;
+  return (
+    <>
+      <canvas ref={canvasRef} />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
+    </>
+  );
 };
-
-export default function Page() {
-  const router = useRouter();
-  base64Code = router.query.base64;
-  decodedCode = Base64DecodeUrl(base64Code);
-  
-  return setupThreeJS();
-}
